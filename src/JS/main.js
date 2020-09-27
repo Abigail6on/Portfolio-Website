aea3bd438d16131da3c71edeba44c37e548228ab
杨漪琳 <yangyilin@rins-MacBook-Pro.local>
2020-09-27 12:47:58 -0400

    add toggle menu js

---
+++ src/JS/main.js
@@ -0,0 +1,29 @@
+//select DOM items
+const menu_button = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menu_nav = document.querySelector('.menu-nav');
+const menu_branding = document.querySelector('.menu-branding');
+const nav_items = document.querySelectorAll('.nav-item');
+
+let show_menu = false;
+menu_button.addEventListener('click', toggle_menu);
+
+function toggle_menu() {
+    if(!show_menu) {
+        menu_button.classList.add('close');
+        menu.classList.add('show');
+        menu_nav.classList.add('show');
+        menu_branding.classList.add('show');
+        nav_items.forEach(item => item.classList.add('show'));
+
+        show_menu = true;
+    } else {
+        menu_button.classList.remove('close');
+        menu.classList.remove('show');
+        menu_nav.classList.remove('show');
+        menu_branding.classList.remove('show');
+        nav_items.forEach(item => item.classList.remove('show'));
+
+        show_menu = false;
+    }
+}
